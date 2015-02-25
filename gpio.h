b2cdaf9c3df6166ba5eb08e5004469e669d645cc
Sumeet Jain <sumeet.jain.engineer@gmail.com>
2015-02-25 16:12:20 +0530

    Update gpio.h
    gpio.h file has been updated.

--- gpio.h
+++ gpio.h
@@ -1,4 +1,15 @@
-raspi_gpio
-==========
+#include <linux/ioctl.h>
+#include <linux/types.h>
 
-GPIO test driver and application for Raspberry Pi
+#define GPIO_IOCTL_BASE 'G'
+
+#define GPIO_SET_VALUE  _IOW(GPIO_IOCTL_BASE, 0, GPIO_USER_DATA)
+#define GPIO_SET_DIRECTION  _IOW(GPIO_IOCTL_BASE, 1, GPIO_USER_DATA)
+#define GPIO_GET_VALUE  _IOR(GPIO_IOCTL_BASE, 2, GPIO_USER_DATA)
+
+typedef struct
+{
+	int gpio_num;
+	int gpio_direction;
+	int gpio_value;
+} GPIO_USER_DATA;
